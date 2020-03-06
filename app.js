de7e402606d2f5e16d940b3f79a53c5acb08ea0a
Weisser <fabian_weisser@arburg.com>
2020-03-06 13:38:36 +0100

    pipeline test 2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from Appsody! Change trigger Pipeline");
+  res.send("Hello from Appsody! Change trigger Pipeline 2!");
 });
  
 module.exports.app = app;
